de94f7ba536aa44863df37f8a5ad73013b8ec32c
Shunsuke Sato <shunsuke.sato26@gmail.com>
2017-05-18 11:58:02 +0200

    Added examples.

---
+++ analysis/epsilon.f90
@@ -0,0 +1,42 @@
+program main
+  implicit none
+  real(8),parameter :: pi = 4d0*atan(1d0)
+  complex(8),parameter :: zI = (0d0,1d0)
+  integer,parameter :: Nt = 6000, Nw = 500
+  real(8),parameter :: wi = 0.1d0/27.211d0, wf = 20d0/27.211d0, dw = (wf-wi)/Nw
+  real(8) :: tt(0:Nt),Act(0:Nt),jt(0:Nt)
+  real(8) :: dt,ww,xx,ff
+  integer :: it,iw
+  complex(8) :: zAw,zEw,zJw,zsigma,zeps
+
+
+  do it = 0,Nt
+    read(*,*)tt(it),Act(it),jt(it)
+  end do
+  dt = tt(1) - tt(0)
+
+  open(21,file="zeps.out")
+  do iw = 0,Nw
+    ww = wi + dw*iw
+    zAw = 0d0; zJw = 0d0
+    do it = 0,Nt
+      xx = (tt(it)-tt(0))/(tt(Nt) - tt(0))
+      ff = 1d0 -3d0 * xx**2 + 2d0*xx**3
+      zAw = zAw + exp(zI*ww*tt(it)) *Act(it)
+      zjw = zjw + exp(zI*ww*tt(it)) *jt(it)*ff
+    end do
+    zEw = -(-zI*ww)*zAw
+    zJw = - zJw
+    zsigma = zJw/zEw
+    zeps = 1d0 + 4d0*pi*zi*zsigma/ww
+
+    write(21,"(999e26.16e3)")ww,zeps,zsigma
+
+  end do
+
+  close(21)
+
+
+
+
+end program main
--- src/global_variables.f90
+++ src/global_variables.f90
@@ -12,18 +12,21 @@ module global_variables
   complex(8),parameter :: zI = (0d0,1d0)
   real(8),parameter :: a_B=0.529177d0,Ry=13.6058d0
 
-! Material parameter
+! Control parameters
+  integer,parameter :: N_PARABOLIC_BAND = 0
+  integer,parameter :: N_NONPARABOLIC_BAND = 1
+
+! Material parameters
   integer :: NKr,NKz
   real(8) :: kr_max,kz_max,dkr,dkz
   real(8),allocatable :: kr(:),kz0(:),kz(:)
-  real(8),parameter :: eps_g = 1.52d0/(2d0*Ry) 
-  real(8),parameter :: mass_r = 1d0/(1d0/0.57d0+1d0/0.067d0)
+  real(8),parameter :: eps_g = 9d0/(2d0*Ry)  !1.52d0/(2d0*Ry) 
+  real(8),parameter :: mass_r = 0.4d0 !1d0/(1d0/0.57d0+1d0/0.067d0)
   real(8),parameter :: piz_vc = 0.5d0*sqrt(eps_g/mass_r)
   real(8),parameter :: fact_intra = 1d0
-  integer,parameter :: nband_type = 0
+  integer,parameter :: nband_type = N_PARABOLIC_BAND !N_NONPARABOLIC_BAND
 
-  integer,parameter :: N_PARABOLIC_BAND = 0
-  integer,parameter :: N_NONPARABOLIC_BAND = 1
+
 
 ! Time-propagation
   integer :: Nt

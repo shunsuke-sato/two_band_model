13ef9734b87472e3d7f0be7fc5be607a1048b55e
Shunsuke Sato <shunsuke.sato26@gmail.com>
2018-04-08 14:03:34 +0200

    Implement Kane band.

--- analysis/sdos.f90
+++ analysis/sdos.f90
@@ -22,18 +22,18 @@ module global_variables
 
 
 ! Discretization
-  integer,parameter :: NKr = 16, NKz = 16
+  integer,parameter :: NKr = 64, NKz = 64
   real(8),parameter :: kz_max = 0.1d0, kr_max = 0.1d0
   real(8),parameter :: dkz = kz_max/Nkz, dkr = kr_max/Nkr
 
 
 ! Fourier transform
   real(8),parameter :: gamma = 0.1d0/ev
-  real(8),parameter :: Tprop = 5d0/gamma
-  real(8),parameter :: dt = 1d0
+  real(8),parameter :: Tprop = 10d0/gamma
+  real(8),parameter :: dt = 10d0
   integer,parameter :: nt = aint(Tprop/dt)+1
   integer,parameter :: Nw = 200
-  real(8),parameter :: wi = 2.5d0/ev, wf = 3.5d0/ev, dw = (wf-wi)/Nw
+  real(8),parameter :: wi = 2.0d0/ev, wf = 4d0/ev, dw = (wf-wi)/Nw
   real(8) :: delta_Dj_w(0:Nw)
 
 ! Laser parameters
@@ -45,6 +45,7 @@ program main
   implicit none
 
   call calc_delta_Dj_parabolic
+  call calc_delta_Dj_kane
 
 
 end program main
@@ -102,4 +103,64 @@ subroutine calc_delta_Dj_parabolic
 
 
 end subroutine calc_delta_Dj_parabolic
+!-------10--------20--------30--------40--------50--------60--------70--------80--------90
+subroutine calc_delta_Dj_kane
+  use global_variables
+  implicit none
+  integer :: iw, ikz, ikr, it
+  real(8) :: ww,kz,kr,eps0, ss,tt
+  real(8) :: a,b,c,d,F0,Ft
+  complex(8) :: ztheta, ztheta0
+
+  delta_Dj_w = 0d0
+
+!$omp parallel default(shared), private(iw,ikz,ikr,it,ww,kz,kr,eps0,a,b,c,d,F0,Ft,tt,ztheta,ztheta0,ss) 
+!$omp do
+  do iw = 0,nw
+    write(*,*)'iw=',iw
+    ww = wi + dw*iw
+
+    do ikz = -nkz,nkz
+      kz = dkz*ikz
+
+      do ikr = 1,nkr
+        kr = dkr*ikr
+
+        eps0 = eps_g*sqrt(1d0  + (kz**2 + kr**2)/(mass_r*eps_g))
+        a = E0**2/(mass_r*eps_g)
+        b = -2d0*kz*E0/(mass_r*eps_g)
+        c = 1d0 + (kz**2+kr**2)/(mass_r*eps_g)
+        d = (b**2 - 4d0*a*c)/(8d0*a)
+        F0 = b*sqrt(c)/(4d0*a)-d*log(abs(b+sqrt(a*c)))/sqrt(a)
+
+        ss = 0d0
+        do it = 0,nt
+          tt = dt*it
+          ztheta0 = -zI*(eps0 - ww)*tt -gamma*tt
+
+          Ft = (2d0*a*tt+b)/(4d0*a)*sqrt(a*tt**2+b*tt+c)-d*log(abs(2*a*tt+b+2*sqrt(a*(a*tt**2+b*tt+c))))/sqrt(a)
+          ztheta  = ztheta0 -zI*(Ft - F0)
+
+          ss = ss + (exp(ztheta) - exp(ztheta0))
+
+        end do
+        ss = ss*dt*dkz*dkr*2d0*pi*kr
+
+        delta_Dj_w(iw) = delta_Dj_w(iw) + ss
+
+      end do
+    end do
+  end do
+!$omp end do
+!$omp end parallel  
+
+  open(20,file='delta_jdos_parabolic_kane.out')
+  do iw =0,nw
+    ww = wi + dw*iw
+    write(20,*)ww,delta_Dj_w(iw)
+  end do
+  close(20)
+
+
+end subroutine calc_delta_Dj_kane
 !-------10--------20--------30--------40--------50--------60--------70--------80--------90
